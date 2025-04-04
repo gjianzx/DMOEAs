3e0713ca3272618e53d0ea34816202b6fb6d0fb4
gjianzx <gjianzx@163.com>
2025-04-04 22:08:37 +0800

    second commit

--- Algorithm/ElseAlgorithm/Algorithms/HEL/RepairBoundary.m
+++
@@ -1,6 +0,0 @@
-function [newPopulationDec] = RepairBoundary(PopulationDec,N,lower,upper)
-    
-    newPopulationDec = max(min(PopulationDec,repmat(upper,N,1)),repmat(lower,N,1));
-    
-end
-
--- Algorithm/ElseAlgorithm/Public/IGD.m
+++
@@ -1,7 +0,0 @@
-function Score = IGD(PopObj,PF)
-% <metric> <min>
-% Inverted generational distance
-
-    Distance = min(pdist2(PF,PopObj),[],2);
-    Score    = mean(Distance);
-end
