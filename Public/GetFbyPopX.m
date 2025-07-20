872cde45564ad653216e77aa6560d278b6480f9b
gjianzx <gjianzx@163.com>
2025-07-20 11:51:59 +0800

    delete public

--- Public/GetFbyPopX.m
+++
@@ -1,8 +0,0 @@
-function PopF = GetFbyPopX(PopX,Problem,t)
-%PopX : n*d
-CostFunction=Problem.FObj;  % Cost Function
-    PopF = [];
-    for i=1:size(PopX,1)
-       PopF(i,:) = CostFunction(PopX(i,:),t);
-    end
-end
